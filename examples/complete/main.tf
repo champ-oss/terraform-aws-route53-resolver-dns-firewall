f239a1b792cbd71dd95b257bb11037579264d74d
Phillip Langley <phillip.langley@champtitles.com>
2026-07-10 13:54:21 -0500

    testing query logs

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -91,7 +91,7 @@ resource "aws_ecs_task_definition" "dns_test" {
       command = [
         "sh",
         "-c",
-        "dnf install -y bind-utils >/dev/null 2>&1 && dig amazonaws.com >/dev/null && dig github.com >/dev/null && dig api.github.com >/dev/null && dig example.com >/dev/null && tail -f /dev/null"
+        "dnf install -y bind-utils >/dev/null 2>&1 && dig amazonaws.com >/dev/null && dig github.com >/dev/null && dig api.github.com >/dev/null && dig www.example.com >/dev/null && tail -f /dev/null"
       ]
     }
   ])
